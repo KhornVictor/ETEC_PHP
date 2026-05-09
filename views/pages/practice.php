3dc9383dbd4750eda03d38393d00ab1f6296a400
victorkhornkh@gmail.com <victorkhornkh@gmail.com>
2026-05-09 14:46:32 +0700

    Quick commit!!!

--- views/pages/practice.php
+++ views/pages/practice.php
@@ -14,11 +14,25 @@
 
         //     echo "</div>";
         // }
-        for ($i = 1; $i <= 100; $i++) {
-            echo "<div class='col-span-1 md:col-span-2 bg-white rounded-lg shadow-md p-4'>";
-            echo "<h2 class='text-xl font-bold mb-4'>Exercise {$i}</h2>";
-            include "app/PHP/practice/ex{$i}.php";
-            echo "</div>";
-        }
+        echo "<div class='col-span-1 md:col-span-2 bg-white rounded-lg shadow-md p-4'>";
+        echo "<h2 class='text-xl font-bold mb-4'>Exercise 1</h2>";
+        include "app/PHP/practice/ex1.php";
+        echo "</div>";
+        echo "<div class='col-span-1 md:col-span-2 bg-white rounded-lg shadow-md p-4'>";
+        echo "<h2 class='text-xl font-bold mb-4'>Exercise 2</h2>";
+        include "app/PHP/practice/ex2.php";
+        echo "</div>";
+        echo "<div class='col-span-1 md:col-span-2 bg-white rounded-lg shadow-md p-4'>";
+        echo "<h2 class='text-xl font-bold mb-4'>Exercise 3</h2>";
+        include "app/PHP/practice/ex3.php";
+        echo "</div>";
+        echo "<div class='col-span-1 md:col-span-2 bg-white rounded-lg shadow-md p-4'>";
+        echo "<h2 class='text-xl font-bold mb-4'>Exercise 4</h2>";
+        include "app/PHP/practice/ex4.php";
+        echo "</div>";
+        echo "<div class='col-span-1 md:col-span-2 bg-white rounded-lg shadow-md p-4'>";
+        echo "<h2 class='text-xl font-bold mb-4'>Exercise 5</h2>";
+        include "app/PHP/practice/ex5.php";
+        echo "</div>";
     ?>
 </div>  
